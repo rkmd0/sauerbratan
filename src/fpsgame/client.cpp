52b22616746f124661d10966709521e2fe5c4e09
rkmd0 <erkamdogan2015@gmail.com>
2023-08-31 07:31:38 +0200

    hideping possibility

--- src/fpsgame/client.cpp
+++ src/fpsgame/client.cpp
@@ -1249,6 +1249,9 @@ namespace game
 
     extern int deathscore;
 
+    VARP(hideping, 0, 0, 1);
+	VARP(pingvalue, INT_MIN, 0, INT_MAX);
+
     void parsemessages(int cn, fpsent *d, ucharbuf &p)
     {
         static char text[MAXTRANS];
@@ -1769,7 +1772,13 @@ namespace game
             }
 
             case N_PONG:
-                addmsg(N_CLIENTPING, "i", player1->ping = (player1->ping*5+totalmillis-getint(p))/6);
+                // wont work if server ignores N_CLIENTPING
+                if(hideping) {
+					getint(p); // ignore value from the server
+					addmsg(N_CLIENTPING, "i", player1->ping = pingvalue);
+				} else {
+                	addmsg(N_CLIENTPING, "i", player1->ping = (player1->ping*5+totalmillis-getint(p))/6);
+				}
                 break;
 
             case N_CLIENTPING:
